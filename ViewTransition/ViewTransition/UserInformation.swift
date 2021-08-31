ae12123ed6271e2576fd419b9a95160b8ab2ae85
전효원 <hyowon612@gmail.com>
2021-08-31 18:21:58 +0900

    ViewTransition

---
+++ ViewTransition/ViewTransition/UserInformation.swift
@@ -0,0 +1,15 @@
+//
+//  UserInformation.swift
+//  ViewTransition
+//
+//  Created by Hyowon on 2021/08/31.
+//
+
+import Foundation
+
+class UserInformation {
+    static let shared: UserInformation = UserInformation()
+    
+    var name: String?
+    var age : String?
+}
